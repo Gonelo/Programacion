6bd0d2c325e54fab2c47c3c42b241fd2a993294f
Gonelo <gonzacornejo360@gmail.com>
2018-05-31 13:00:35 -0300

    first commit

--- fibonacci.c
+++ fibonacci.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 int main() {
-	printf("%i\n", fibonacci(4));
+	int a = -1;
+	if (a<1){
+		printf("Che la cagaste\n");
+	}
+	if (a>=1){
+		printf("%i\n", fibonacci(a));
+	}
 	return 0;
 
 }
 
 int fibonacci(int a){
-	if (a==0){
+	if (a<1){
+		return -1;
+	}
+	if (a == 1) {
 		return 0;
 	}
-	if (a==1){
+	if (a == 2) {
 		return 1;
 	}
 	int rta = 0;
 	int x = 0;
 	int y = 1;
 	int cont;
-	if (a>1){
-		for (cont=1;cont<a;cont=cont+1){
+	if (a > 2) {
+		for (cont = 1; cont < a; cont = cont + 1) {
 			rta = x+y;
 			x = y;
 			y = rta;
